ef5243b87e910f2d86693883bc3ef4ed048d0837
garreth.smith <garreth.smith@UK-LBHRTQF2.groupinfra.com>
2017-02-08 17:28:47 +0000

    Datasource

--- tournament/src/main/java/com/fantasy/dao/AccountDAO.java
+++ tournament/src/main/java/com/fantasy/dao/AccountDAO.java
@@ -2,15 +2,19 @@ package com.fantasy.dao;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.DispatcherServlet;
+
 
 public class AccountDAO {
 
 	JdbcTemplate jdbcTemplate;
 	
-	
+
 	public void setDatasource(DataSource datasource) {
-		this.jdbcTemplate = new JdbcTemplate(datasource);
+		//this.jdbcTemplate = new JdbcTemplate(datasource);
 	}
 
 }
